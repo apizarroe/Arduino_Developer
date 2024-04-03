e34ce325d44650dd52065f7fad4ec1f6d44fa0a0
apizarroe <apizarroe13@gmail.com>
2024-04-03 02:04:29 -0500

    Update Proyecto 2024-04-03
    Update Proyecto 2024-04-03

--- final_proyecto_certificador2/final_proyecto_certificador2.ino
+++ final_proyecto_certificador2/final_proyecto_certificador2.ino
@@ -8,17 +8,30 @@
 #include <DallasTemperature.h>
 #include <IRremote.h>
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 
 const int RECV_PIN=4;
 const int IR_SEND_PIN=5;
-const int ledPin = D0;
+const int led001Pin = D0;
+const int led002Pin = D1;
+const int mot001APin = D2;
+const int mot001BPin = D3;
+const int mot001VelPin = D4;
+const int mot002VelPin = D6;
+const int tmp001Pin = D5;
+const int blue001RX = D7;
+const int blue001TX = D8;
+
 //Se establece el pin 4 como bus OneWire
 //Se declara una variable para nuestro sensor
-OneWire ourWire(RECV_PIN);
+
+SoftwareSerial bluetoothSerial(blue001RX, blue001TX);
+OneWire ourWire(tmp001Pin);
 DallasTemperature sensors(&ourWire);
-float t;
-unsigned int wifi_attempts = 0, thing_attempts = 0, aws_attempts = 0, snp_attempts = 0;
+
+float t, temper_global=0;
+unsigned int wifi_attempts = 0, thing_attempts = 0, aws_attempts = 0, snp_attempts = 0, ventil_manual=0;
 unsigned long lastMillis = 0, previousMillis = 0;
 const long interval = 5000;
  
@@ -151,17 +164,59 @@ time_t stringToTime(const char* dateTimeString) {
     // Convertir el objeto tm en segundos desde el 1 de enero de 1970
     return mktime(&tm);
 }
+
+void MotorEncendidoFull()
+{
+  analogWrite(mot001VelPin, 255);
+}
+
+void MotorEncendidoMedio()
+{
+  analogWrite(mot001VelPin, 180);
+}
+
+void MotorApagado()
+{
+  analogWrite(mot001VelPin, 0);
+}
+
+void PersianaCerrar()
+{
+  digitalWrite(mot001APin, HIGH);
+  digitalWrite(mot001BPin, LOW);
+  analogWrite(mot002VelPin, 170);
+}
+
+void PersianaAbrir()
+{
+  digitalWrite(mot001APin, LOW);
+  digitalWrite(mot001BPin, HIGH);
+  analogWrite(mot002VelPin, 170);
+}
+
+void PersianaDetener()
+{
+  digitalWrite(mot001APin, LOW);
+  digitalWrite(mot001BPin, LOW);
+  analogWrite(mot002VelPin, 0);
+}
  
 void setup()
 {
   Serial.begin(9600);
+  bluetoothSerial.begin(9600);
   //IrReceiver.begin(RECV_PIN, ENABLE_LED_FEEDBACK);
   //IrSender.begin(IR_SEND_PIN);
   connectWiFi();
   NTPConnect();
   connectAWS();
   //dht.begin();
-  pinMode(ledPin, OUTPUT);
+  pinMode(led001Pin, OUTPUT);
+  pinMode(led002Pin, OUTPUT);
+  pinMode(mot001APin, OUTPUT);
+  pinMode(mot001BPin, OUTPUT);
+  pinMode(mot001VelPin, OUTPUT);
+  pinMode(mot002VelPin, OUTPUT);
 }
   
 void loop()
@@ -181,7 +236,13 @@ void loop()
     //Serial.println(timeSinceEpoch_fin);
     //Serial.println(now);
     if (timeSinceEpoch_inicio < now and now < timeSinceEpoch_fin){
-      digitalWrite(ledPin, HIGH);
+      if(tipo == "Fiesta"){
+        digitalWrite(led002Pin, HIGH);
+        digitalWrite(led001Pin, LOW);
+      } else if(tipo == "Pelicula") {
+        digitalWrite(led001Pin, HIGH);
+        digitalWrite(led002Pin, HIGH);
+      }
     }
 
     if (now > timeSinceEpoch_fin){
@@ -194,9 +255,74 @@ void loop()
       tipo="";
       medio="";
       contenido="";
-      digitalWrite(ledPin, LOW);
+      digitalWrite(led001Pin, LOW);
+      digitalWrite(led002Pin, LOW);
     }
   }
 
+  sensors.requestTemperatures();   //Se envía el comando para leer la temperatura
+  float temp= sensors.getTempCByIndex(0); //Se obtiene la temperatura en ºC
+
+  /*if(temp != 0 and temper_global != temp){
+    Serial.print("Temperatura= ");
+    Serial.print(temp);
+    Serial.println(" C");
+  }*/
+
+  if(now % 5 == 0){
+    if(temp != temper_global) {
+      Serial.print("Temperatura= ");
+      Serial.print(temp);
+      Serial.println(" C");
+    }
+    if(ventil_manual == 0){
+      if (temp <= 25){
+        Serial.println("Apaga motor");
+        MotorApagado();
+      } else if(temp > 25 and temp <=30){
+        Serial.println("Enciende motor, velocidad media");
+        MotorEncendidoMedio();
+      } else if (temp > 30){
+        Serial.println("Enciende motor, velocidad alta");
+        MotorEncendidoFull();
+      }
+    }
+    temper_global = temp;
+  }
+
+  if (bluetoothSerial.available()) {
+    char receivedChar = bluetoothSerial.read();
+    if(receivedChar == '1')	{
+      Serial.print("Cerrando Persiana");
+      PersianaCerrar();
+    }
+    if(receivedChar == '2')	{
+      Serial.print("Abriendo Persiana");
+      PersianaAbrir();
+    }
+    if(receivedChar == '3')	{
+      Serial.print("Detener Persiana");
+      PersianaDetener();
+    }
+    if(receivedChar == '4')	{
+      Serial.print("Detener Ventilador Manual");
+      ventil_manual = 1;
+      MotorApagado();
+    }
+    if(receivedChar == '5')	{
+      Serial.print("Iniciar Ventilador Manual");
+      ventil_manual = 1;
+      if(temp <=30){
+        MotorEncendidoMedio();
+      } else {
+        MotorEncendidoFull();
+      }
+    }
+    if(receivedChar == '6')	{
+      Serial.print("Ventilador Modo Automatico");
+      ventil_manual = 0;
+    }
+  }
+  
   client.loop();
 }
